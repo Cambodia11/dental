ede8cb3bf8bca9717751e294f7414b8fee975feb
Dupluk1 <121636465+Dupluk1@users.noreply.github.com>
2025-09-23 14:09:11 +0300

    Подключил Postgres через dj-database-url

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,11 +80,14 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),  # Render возьмет эту переменную
+        conn_max_age=600,  # держим соединение открытым (чтобы не было лагов)
+        ssl_require=True   # для Render обязательно включаем SSL
+    )
 }
 
 
